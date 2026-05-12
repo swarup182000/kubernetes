721206cbe34f6f7236406d067f1f1075e9413596
Ubuntu <ubuntu@ip-172-31-10-0.ap-south-1.compute.internal>
2026-05-12 23:18:56 +0000

    Updated app to v2

--- practice-app/index.js
+++ practice-app/index.js
@@ -1,10 +1,10 @@
+cat <<EOF > index.js
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Swarup DevOps App! v1</h1>');
+  res.send('<h1>Hello from Swarup DevOps App! v2</h1>');
 });
-
 app.listen(3000, () => {
   console.log('App running on port 3000');
 });
+EOF
